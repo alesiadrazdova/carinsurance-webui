775fa6b28a8621eae8de6cf914511bdd56ad12a3
adrazdova <alesiadrozdova@gmail.com>
2023-05-15 17:21:17 +0200

    Vuetify installed successfully

--- carinsurance-webui/src/App.vue
+++ carinsurance-webui/src/App.vue
@@ -1,30 +1,20 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <v-sheet width="300" class="mt-16 mx-auto">
+    <v-form @submit.prevent>
+      <v-text-field
+        v-model="firstName"
+        :rules="rules"
+        label="Name"
+      ></v-text-field>
+      <v-text-field
+        v-model="firstName"
+        :rules="rules"
+        label="Password"
+      ></v-text-field>
+      <v-btn type="submit" block class="mt-2" color="secondary">Submit</v-btn>
+    </v-form>
+  </v-sheet>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<script setup>
+</script>
--- carinsurance-webui/src/main.js
+++ carinsurance-webui/src/main.js
@@ -1,5 +1,20 @@
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
+import { loadFonts } from './plugins/webfontloader'
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
 
-createApp(App).mount('#app')
+const vuetify = createVuetify({
+  components,
+  directives,
+})
+
+loadFonts()
+
+
+
+createApp(App)
+  .use(vuetify)
+  .mount('#app')
