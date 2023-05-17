d1dcf830ac7a14dfff7289682741e8be67813ac5
adrazdova <alesiadrozdova@gmail.com>
2023-05-17 22:22:48 +0200

    Initial commit

---
+++ src/views/Home.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-sheet width="300" class="mt-16 mx-auto">
+    <v-form @submit.prevent>
+      <v-text-field
+        label="Name"
+      ></v-text-field>
+      <v-text-field
+        label="Password"
+      ></v-text-field>
+      <v-btn type="submit" block class="mt-2" color="secondary">Submit</v-btn>
+    </v-form>
+  </v-sheet>
+</template>
