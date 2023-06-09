5eddac3a0a165f888a4d5874ddfb95f98a3d6fd6
adrazdova <alesiadrozdova@gmail.com>
2023-06-09 17:44:07 +0200

    added new fields

--- src/components/popups/NewCase.vue
+++ src/components/popups/NewCase.vue
@@ -1,8 +1,13 @@
 <script lang="ts">
 import { defineComponent, ref } from 'vue'
 import InsuranceCase from '@/types/InsuranceCase'
+import VueDatePicker from '@vuepic/vue-datepicker'
+import '@vuepic/vue-datepicker/dist/main.css'
 
 export default defineComponent({
+  components: {
+    VueDatePicker
+  },
   props: {
     cases: {
       type: Array as () => InsuranceCase[],
@@ -11,6 +16,7 @@ export default defineComponent({
   },
   setup() {
     const dialogVisible = ref(false)
+    const date = ref(new Date())
     const user = ref({
       firstName: '',
       lastName: '',
@@ -29,7 +35,7 @@ export default defineComponent({
       firstNameThirdParty: '',
       lastNameThirdParty: '',
       phoneNumberThirdParty: '',
-      images: []
+      images: [],
     })
 
     const closeDialog = () => {
@@ -51,6 +57,7 @@ export default defineComponent({
       closeDialog,
       submitForm,
       openCreateCaseDialog,
+      date
     }
   }
 })
@@ -69,7 +76,7 @@ export default defineComponent({
         rounded
         size="large"
       >
-      Create new claim
+        Create new claim
       </v-btn>
     </template>
     <v-card class="pb-2">
@@ -101,7 +108,6 @@ export default defineComponent({
             >
               <v-text-field
                 label="Last name*"
-                persistent-hint
                 required
                 v-model="user.lastName"
                 prepend-inner-icon="mdi-account"
@@ -114,7 +120,7 @@ export default defineComponent({
             >
               <v-text-field
                 label="Phone number"
-                hint="example of helper text only on focus"
+                required
                 v-model="user.phoneNumber"
                 prepend-inner-icon="mdi-phone"
               ></v-text-field>
@@ -162,7 +168,7 @@ export default defineComponent({
             >
               <v-text-field
                 label="Car make*"
-                hint="example of helper text only on focus"
+                required
                 v-model="user.carMake"
                 prepend-inner-icon="mdi-car"
               ></v-text-field>
@@ -224,6 +230,7 @@ export default defineComponent({
             >
               <v-text-field
                 prepend-inner-icon="mdi-map-marker"
+                required
                 label="Accident location*"
                 v-model="user.accidentLocation"
               ></v-text-field>
@@ -233,11 +240,16 @@ export default defineComponent({
               sm="6"
               md="4"
             >
-              <v-text-field
-                label="Accident date and time*"
-                v-model="user.dateAndTimeAccident"
-                prepend-inner-icon="mdi-clipboard-text-clock"
-              ></v-text-field>
+              <vue-date-picker
+                v-model="date"
+                :style="{ '--dp-input-padding' : '15px 0', '--dp-input-icon-padding' : '47px' }"
+                class="date-picker"
+                required
+              >
+                  <template #input-icon>
+                    <v-icon class="px-4">mdi-clock-time-eight</v-icon>
+                  </template>
+              </vue-date-picker>
             </v-col>
             <v-col cols="12">
               <v-textarea
@@ -269,7 +281,6 @@ export default defineComponent({
             >
               <v-text-field
                 label="First name*"
-                required
                 v-model="user.firstNameThirdParty"
                 prepend-inner-icon="mdi-account"
               ></v-text-field>
@@ -281,8 +292,6 @@ export default defineComponent({
             >
               <v-text-field
                 label="Last name*"
-                persistent-hint
-                required
                 v-model="user.lastNameThirdParty"
                 prepend-inner-icon="mdi-account"
               ></v-text-field>
@@ -329,3 +338,14 @@ export default defineComponent({
   </v-dialog>
 </template>
 
+<style scoped lang="scss">
+.date-picker {
+  border-bottom: 1px solid #A5A5A5;
+}
+.dp__theme_light {
+  --dp-border-color: none;
+  --dp-border-color-hover: none;
+  --dp-background-color: #F6F6F6;
+  --db-background-color-hover: #EDEDED;
+}
+</style>
